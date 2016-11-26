1a333c1d4dd008f2c60c15663d60a7dc195d44a9
=Sairam Rakshith Bhyravabhotla <=bsairamr@iu.edu>
2016-11-26 03:06:25 +0000

    tt split done

--- Task2/Preprocessing.py
+++ Task2/Preprocessing.py
@@ -4,15 +4,16 @@ This file includes data cleaning and preprocessing
 '''
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 
 def read_data(file_path):
 	return pd.read_csv(file_path)
 
 def split_data(data):
-	return train_test_split(data, train_size = 0.7)
+	return train_test_split(data)
 
 data = read_data("./ReviewData.csv")
 train_data, test_data = split_data(data)
-print len(data) , len(train_data), len(test_data) , type(train_data)
-
+del data
+train_data.to_csv("train.csv", sep=",")
+test_data.to_csv("test.csv", sep=",")
